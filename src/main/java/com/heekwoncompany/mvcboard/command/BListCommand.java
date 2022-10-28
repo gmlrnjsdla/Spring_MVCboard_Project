c4889bd5314a10585a3ac0bfaa3c1f535dd1f84e
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-28 15:47:08 +0900

    DB연동//게시판 글목록 제작

--- src/main/java/com/heekwoncompany/mvcboard/command/BListCommand.java
+++ src/main/java/com/heekwoncompany/mvcboard/command/BListCommand.java
@@ -1,13 +1,22 @@
 package com.heekwoncompany.mvcboard.command;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
+
+import com.heekwoncompany.mvcboard.dao.BoardDao;
+import com.heekwoncompany.mvcboard.dto.BoardDto;
 
 public class BListCommand implements BCommand {
 
 	@Override
 	public void excute(Model model) {
 		
-
+		BoardDao dao = new BoardDao();
+		
+		ArrayList<BoardDto> dtos = dao.list();
+		//DB 에서 가져온 글 목록을 모두가지고 있는 ArrayList 자료구조의 dtos를 저장
+		
+		model.addAttribute("list", dtos); //글 목록 ArrayList 모델객체안에 저장
 	}
-
 }
--- src/main/java/com/heekwoncompany/mvcboard/controller/BoardController.java
+++ src/main/java/com/heekwoncompany/mvcboard/controller/BoardController.java
@@ -1,15 +1,22 @@
 package com.heekwoncompany.mvcboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.heekwoncompany.mvcboard.command.BCommand;
+import com.heekwoncompany.mvcboard.command.BListCommand;
 
 @Controller
 public class BoardController {
-
+	
+	BCommand command = null;
+	
 	@RequestMapping(value="list")
-	public String list() {
+	public String list(Model model) {
 		
-		
+		command = new BListCommand();
+		command.excute(model);
 		
 		return "list";
 	}
